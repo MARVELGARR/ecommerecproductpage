3c8ad2a98493ac9bbaf2ec51a3277b6d8be96c25
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2023-10-22 23:02:11 +0100

    added the backdrop

--- src/components/utility component/body components/ImagePreview.tsx
+++ src/components/utility component/body components/ImagePreview.tsx
@@ -21,16 +21,16 @@ const ImagePreview: React.FC = () =>{
                 return (
                     <div className=" flex justify-between gap-14">
 
-                        <div key={item.id} className=" min-w-[330px]  flex flex-col gap-3">
+                        <div key={item.id} className=" min-w-[430px] min-h-[430px]  flex flex-col gap-3">
                             <div className="">
                                 {item?.imageArray?.map((image)=>{
                                     return (
-                                        <div key={image.id} className={`relative ${ data == image.id ? "" : "hidden" }`}>
+                                        <div key={image.id} className={` w-full relative ${ data == image.id ? "" : "hidden" }`}>
 
                                             <img 
                                                 src={image.src}
                                                 alt={image.alt}
-                                                className=' flex max-w-full h-[22rem] rounded-xl'
+                                                className=' flex w-full h-[22rem] rounded-xl'
                                             />
                                             <div onClick={()=>dispatch(notViewing())} className="absolute top-0 -right-10 z-50">
                                                 <img
@@ -44,14 +44,14 @@ const ImagePreview: React.FC = () =>{
                                     )
                                 })}
                             </div>
-                            <div className="flex gap-5">
+                            <div className="flex w-full gap-5">
                                 {item.images?.map((imageItems)=>{
                                     return(
-                                        <div onClick={()=>handleSelect(imageItems.id)} key={imageItems.id} className={` cursor-pointer ${data == parseInt(imageItems.id) ? " filter bg-orange-400 opacity-60 ring-2 ring-orange-600" : ""} rounded-lg`}>
+                                        <div onClick={()=>handleSelect(imageItems.id)} key={imageItems.id} className={` w-full cursor-pointer ${data == parseInt(imageItems.id) ? " filter bg-orange-400 opacity-60 ring-2 ring-orange-600" : ""} rounded-lg`}>
                                             <img 
                                                 src={imageItems.src}
                                                 alt={imageItems.alt}
-                                                className=' filter brightness-110 rounded-lg w-16 h-14'
+                                                className=' filter brightness-110 rounded-lg  w-full h-14'
                                             />
                                         </div>
                                     )
